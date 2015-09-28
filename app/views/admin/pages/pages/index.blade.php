b9c9b3d676333d54a244274e133d355fd52f8599
Stef van den Berg <stef@debmedia.nl>
2015-09-29 00:53:19 +0200

    This should do the trick

--- app/views/admin/pages/pages/index.blade.php
+++ app/views/admin/pages/pages/index.blade.php
@@ -53,8 +53,8 @@
                         </div>
                     </td>
                 </tr>
-                @if (count($page->children()) > 0)
-                    @foreach($page->children() as $child)
+                @if (count($page->children()->get()) > 0)
+                    @foreach($page->children()->get() as $child)
                         <tr class="children">
                             <td>{{$child->id}}</td>
                             <td>{{$child->title}}</td>
